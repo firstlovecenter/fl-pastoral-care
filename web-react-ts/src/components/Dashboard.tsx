f4c6957e3547a4c9cfe7b8c9c8cc82e1f08372c1
John-Dag Addy <jaedagy@gmail.com>
2022-03-31 09:28:35 +0000

    feat: introduced a cloudinary image component with support for lazy loading and placeholders

--- web-react-ts/src/components/Dashboard.tsx
+++ web-react-ts/src/components/Dashboard.tsx
@@ -1,10 +1,12 @@
 import { Container } from '@chakra-ui/react'
 import Login from './Login'
+import CloudinaryImage from './CloudinaryImage'
 
 const Dashboard = () => {
   return (
     <Container>
       Dashboard
+      <CloudinaryImage />
       <Login />
     </Container>
   )
